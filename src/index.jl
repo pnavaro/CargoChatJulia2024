3900d6b2bc17929fe3c7c0aedb775d78941bf962
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-02-02 19:31:51 +0100

    Ajout de quelques exemples

--- src/index.jl
+++ src/index.jl
@@ -2,11 +2,18 @@
 
 #md # # CargoChat Julia 5 avril 2024
 #md #
-#md #  - *Pierre Navaro*
+#md # ## Pierre Navaro
 #md #
-#md #  - Ingénieur Calcul à l'IRMAR
+#md # Ingénieur Calcul à l'IRMAR (Institut de Recherche Mathématique de Rennes)
 #md #
-#md #  https://pnavaro.github.io/CargoChatJulia2024
+#md # Supports : https://pnavaro.github.io/CargoChatJulia2024
+#md #
+#md # Membre du [Groupe Calcul](https://calcul.math.cnrs.fr)
+#md #
+#md # Formation : Thèse de doctorat en Mécanique des fluides numerique
+#md #
+#md # Langages : Fortran, Python, Julia et un peu de R
+#md #
 #md #
 
 #md # ---
@@ -20,62 +27,203 @@
 #md # ---
 
 #md #
-#md # # Pourquoi Julia?
+#md # # Un peu de bibliographie
 #md #
-#md # - Démarré en 2009,  première version en 2012 et version stable depuis 2018.
-#md # - Les langages comme Python et R sont faciles à prendre en main mais sont parfois lents.
-#md # - Les langages comme Fortran et C/C++ sont plus difficiles à utiliser mais sont rapides.
-#md # - Julia propose d'éliminer "le problème des deux langages" pour offrir les meilleur des deux mondes.
+#md # L'article des créateurs:
 #md #
 #md # **Julia: A Fresh Approach to Numerical Computing**
 #md # 
-#md # *Jeff Bezanson, Alan Edelman, Stefan Karpinski, Viral B. Shah*
+#md # *Jeff Bezanson, Alan Edelman, Stefan Karpinski, Viral B. Shah*  SIAM Rev., 59(1), 65–98. (34 pages) 2012
+#md #
+#md # Sources des images : [Matthijs Cox - My Target Audience](https://scientificcoder.com/my-target-audience#heading-the-two-culture-problem)
 #md # 
-#md # SIAM Rev., 59(1), 65–98. (34 pages) 2012
+#md # [Julia Hub](https://juliahub.com/) : le portail Julia pour éxécuter des notebooks et voir tous les packages existants.
+#md # 
+#md # Le livre blanc : [Why Julia ?](https://juliahub.com/assets/pdf/why_julia.pdf)
+#md # 
+#md # Un article en français : [A la découverte de Julia](https://zestedesavoir.com/articles/pdf/78/a-la-decouverte-de-julia.pdf)
+#md # 
+#md # Liste du CNRS avec la newsletter  https://github.com/pnavaro/NouvellesJulia
+#md # 
+#md # Liste rennaise pour les ateliers et les formations https://github.com/pnavaro/math-julia
+#md # 
 
 
 #md # ---
 
-#md # # Les particularités de Julia
-#md #
-#md # - Les types composés ne pourront pas être modifiés dans une même session, peu pratique mais offre une meilleure gestion de la mémoire.
-#md # - Toutes les fonctions sont compilés lors de leur premier appel, un peu de lattence mais l'exécution est beaucoup plus rapide ensuite.
-#md # - Les fonctions sont spécialisées suivant le type de leurs arguments, plus de compilation mais cela améliore la lisibilité de vos interfaces.
-#md # - Julia est interactif, facile à utiliser mais la création de binaires est compliquée.
-#md # - Julia possède plusieurs manières pour manipuler les tableaux, très pratique pour optimiser son empreinte mémoire mais nécessite un apprentissage.
-#md # - La bibliothèque standard et quasiment tous les packages sont écrits en Julia, il est facile de s'en inspirer.
-#md # - Il y a un mécanisme de géneration de code et de méta-programmation qui permet de faire de très belles interfaces, il nécessite des compétences avancées.
-#md #
+#md # # Une fonction Julia
+
+f(x) = 2x^2
+
+f(3)
+
+#md # Les fonctions sont compilées lors du premier appel suivant le type de ses arguments
+
+#md # ```julia
+#md # @code_llvm f(3)
+#md # 
+#md # ;  @ In[22]:1 within `f`
+#md # define i64 @julia_f_1553(i64 signext %0) #0 {
+#md # top:
+#md # ; ┌ @ intfuncs.jl:332 within `literal_pow`
+#md # ; │┌ @ int.jl:88 within `*`
+#md #     %1 = shl i64 %0, 1
+#md # ; └└
+#md # ; ┌ @ int.jl:88 within `*`
+#md #    %2 = mul i64 %1, %0
+#md # ; └
+#md #   ret i64 %2
+#md # }
+#md # ```
 
 #md # ---
 
+#md # la fonction `f` peut accepter une matrice comme argument
+
+A = rand(3,3)
+
+f(A)
+
+#md # la fonction `f` peut également opérer sur tous les éléments d'un tableau
+
+b = [1, 2, 3]
+
+map(f, b)
+
+#md #
+
+f.(b)
+
+
+#md # ---
+
+#md # # Algèbre linéaire
+
+using LinearAlgebra
+
+X = hcat(ones(3), rand(3), rand(3))
+y = collect(1:3)
+
+β = inv(X'X) * X'y
+
+#md #
+
+β = X \ y
+
+#md # ---
+
+#md # # Matrices creuses
+
+using SparseArrays
+
+rows = [1,3,4,2,1,3,1,4,1,5]
+cols = [1,1,1,2,3,3,4,4,5,5]
+vals = [5,-2,-4,5,-3,-1,-2,-10,7,9]
+
+A = sparse(rows, cols, vals, 5, 5)
+
+#md #
+
+b = collect(1:5)
+
+A \ b
+
+#md # ---
+
+#md # # Création d'un type Julia
+
+"""
+Nombre rationnel
+"""
+struct MyRational
+    n :: Int
+    d :: Int
+
+    function MyRational(n :: Int, d :: Int) 
+        @assert d != "zero denominator"
+        g = gcd(n,d)
+        new( n ÷ g, d ÷ g)
+    end
+end
+
+a = MyRational(3, 4)
+
+#md #
+
+Base.show(io::IO, r::MyRational) = print(io, "$(r.n) // $(r.d)")
+
+b = MyRational(2, 3)
+
+#md # ---
+
+import Base.+
+function +(a::MyRational, b::MyRational)
+    MyRational(a.n*b.d+b.n*a.d, a.d*b.d)
+end
+
+a + b
+
+#md # ---
 
 #md # # Générer des données bruitées
 
 using Random
 
+function generate_data( rng, weights, bias; num_samples = 100, noise = 0.01)
+
+    num_features = length(weights)
+    x = randn(rng, (num_features, num_samples))  # création des variables explicatives aléatoires
+    y =  x' * weights .+ bias .+ noise .* randn(rng, num_samples)
+
+    return x, y
+
+end
+
 rng = Xoshiro(1234)
 
-num_samples, num_features = 100, 2
-
-x = randn(rng, (num_features, num_samples))
-
-weights = [0.5, 0.1]
-
+weights = [0.1, 0.2, 0.3, 0.4, 0.5 ]
 bias = 0.2
 
-noise = 0.01 .* randn(rng, num_samples)
-
-y =  x' * weights .+ bias .+ noise
+x, y =  generate_data( rng, weights, bias)
 
 #md # ---
 
-#md # ## Fonction pour une regression linéaire
+#md # ## Fonction non typée pour une regression linéaire
 
 function linear_regression( x, y; learning_rate = 0.01, iterations = 1000)
         
-    T = eltype(x)
-    @assert eltype(y) <: T
+    num_features, num_samples  = size(x)
+    weights = ones(num_features)
+    bias = 0.0
+    
+    for i in 1:iterations
+        
+        y_pred =  x' * weights .+ bias
+        dw = x * ( y_pred .- y ) ./ num_samples
+        db = sum(y_pred .- y ) ./ num_samples
+        weights .-= learning_rate .* dw
+        bias -= learning_rate * db
+        
+    end
+    
+    return weights, bias
+        
+end
+
+#md # ---
+
+linear_regression( x, y)
+
+#md #
+
+linear_regression( Float32.(x), Float32.(y))
+
+#md # ---
+
+#md # ## Fonction typée pour une regression linéaire
+
+function linear_regression( x::Matrix{T}, y::Vector{T}; learning_rate = 0.01, iterations = 1000) where T
+        
     num_features, num_samples  = size(x)
     weights = ones(T, num_features)
     bias = zero(T)
@@ -102,11 +250,7 @@ linear_regression( x, y)
 
 linear_regression( Float32.(x), Float32.(y))
 
-#md # 
-
-linear_regression( Float16.(x), Float16.(y))
-
-#md # ---
+#md # --- 
 
 #md # ## Type composé pour une régression linéaire
 
@@ -117,7 +261,7 @@ struct LinearRegression{T}
    weights :: Vector{T}
    bias :: T
     
-   function LinearRegression( x :: Matrix{T}, y; learning_rate = 0.01, iterations = 1000) where T
+   function LinearRegression( x :: Matrix{T}, y :: Vector{T}; learning_rate = 0.01, iterations = 1000) where T
         
         num_features, num_samples  = size(x)
         weights = ones(T, num_features)
@@ -128,8 +272,8 @@ struct LinearRegression{T}
             y_pred =  x' * weights .+ bias
             dw = x * ( y_pred .- y ) ./ num_samples
             db = sum(y_pred .- y ) ./ num_samples
-            weights .-= learning_rate .* dw
-            bias -= learning_rate * db
+            weights .-= T(learning_rate) .* dw
+            bias -= T(learning_rate) * db
             
         end
         
@@ -140,17 +284,42 @@ struct LinearRegression{T}
 end
     
 
+#md # --- 
 
 model = LinearRegression(x, y)
 
 model.weights
 
+#md #
+
 model.bias
+
+#md #
 
 predict(model, x) = x' * model.weights .+ model.bias
 
 function Base.show(io :: IO, model :: LinearRegression) 
-    println("Linear Regression")
-    println("weights : $(model.weights)")
-    println("bias : $(model.bias)")
+    println(io, "Linear Regression")
+    println(io, "=================")
+    println(io, "weights : $(round.(model.weights, digits=3))")
+    println(io, "bias : $(round(model.bias, digits=3))")
 end
+
+model
+
+#md #
+
+#md # ---
+
+#md # # Les particularités de Julia
+#md #
+#md # - Les types composés ne pourront pas être modifiés dans une même session, peu pratique mais offre une meilleure gestion de la mémoire.
+#md # - Toutes les fonctions sont compilés lors de leur premier appel, un peu de lattence mais l'exécution est beaucoup plus rapide ensuite.
+#md # - Les fonctions sont spécialisées suivant le type de leurs arguments, plus de compilation mais cela améliore la lisibilité de vos interfaces.
+#md # - Julia est interactif, facile à utiliser mais la création de binaires est compliquée.
+#md # - Julia possède plusieurs manières pour manipuler les tableaux, très pratique pour optimiser son empreinte mémoire mais nécessite un apprentissage.
+#md # - La bibliothèque standard et quasiment tous les packages sont écrits en Julia, il est facile de s'en inspirer.
+#md # - Il y a un mécanisme de géneration de code et de méta-programmation qui permet de faire de très belles interfaces, il nécessite des compétences avancées.
+#md #
+
+#md # ---
